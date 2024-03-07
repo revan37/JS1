fc40971c02886b36c951f813caa0ea22f3cbed0a
revan37 <151071347+revan37@users.noreply.github.com>
2024-03-07 15:54:46 +0400

    JS1

---
+++ app.js
@@ -0,0 +1,8 @@
+var name = prompt("Adin nedi?"); //string
+var surname = prompt("soyadin nedi?");//string
+var age = prompt("yasin necedi?");
+console.log(typeof age);
+console.log(surname,name,age);
+//console.log(18-age);
+//var galib = 18- age;
+//console.log("salam" + surname + " " +name +" , senin 18 yasina " +qalib +" il qalib ");
