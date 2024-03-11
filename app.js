1b8da073fac44ec20abf2f3f1584ec0f018d8d1f
revan37 <151071347+revan37@users.noreply.github.com>
2024-03-11 15:30:10 +0400

    date

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-var name = prompt("Adin nedi?"); //string
-var surname = prompt("soyadin nedi?");//string
-var age = prompt("yasin necedi?");
-console.log(typeof age);
-console.log(surname,name,age);
+//var name = prompt("Adin nedi?"); //string
+//var surname = prompt("soyadin nedi?");//string
+//var age = prompt("yasin necedi?");
+//console.log(typeof age);
+//console.log(surname,name,age);
 //console.log(18-age);
 //var galib = 18- age;
-//console.log("salam" + surname + " " +name +" , senin 18 yasina " +qalib +" il qalib ");
+//console.log("salam" + surname + " " +name +" , senin 18 yasina " + qalib +" il qalib ");
+var age = prompt("nece il?")
+age = 2024 - age;
+console.log(age * 365);
+console.log(age * 365 * 24);
+console.log(age * 365 * 24 * 60);
+console.log(age * 365 * 24 * 60 * 60);
+console.log(age * 365 * 24 * 60 * 60 * 1000);
